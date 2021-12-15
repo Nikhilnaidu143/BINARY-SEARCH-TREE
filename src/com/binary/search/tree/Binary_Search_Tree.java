aa5f3abfa10f032e81ace04aacb2347e30023bf4
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-15 16:33:15 +0530

    START:- Displayed Welcome Message.

---
+++ src/com/binary/search/tree/Binary_Search_Tree.java
@@ -0,0 +1,7 @@
+package com.binary.search.tree;
+
+public class Binary_Search_Tree {
+	public static void main(String[] args) {
+		System.out.println("-----------WELCOME TO BINARY-SEARCH-TREE-----------\n");
+	}
+}
